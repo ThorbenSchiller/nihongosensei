15c6a259e8752fdff975f8357d14d0bfa388ad25
Thorben Schiller <schillerthorben@gmail.com>
2021-07-17 19:39:38 +0200

    feat: use full text search

--- services/VocabularyService/findByQuery.ts
+++ services/VocabularyService/findByQuery.ts
@@ -9,8 +9,11 @@ export async function findByQuery(
 ): Promise<EntryModel[]> {
   const { limit = 15, offset = 0 } = options;
   const results = await execute<EntryWrapperModel>(
-    `SELECT * FROM entry WHERE JSON_SEARCH(entry_json, 'all', ?) LIMIT ?, ?`,
-    [query, offset, Math.max(limit, MAX_LIMIT)]
+    `SELECT * FROM entry
+    WHERE
+        MATCH(text_plain, hiragana_plain, orths_plain, senses_plain)
+        AGAINST (? IN BOOLEAN MODE) LIMIT ?, ?`,
+    [`${query}*`, offset, Math.max(limit, MAX_LIMIT)]
   );
 
   return results.map((result) => result.entry_json);
