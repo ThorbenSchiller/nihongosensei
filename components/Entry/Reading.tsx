11b44e5172dae76322bcadac7b28661fcf9c4fce
Thorben Schiller <schillerthorben@gmail.com>
2021-07-18 21:30:48 +0200

    fix: replace backslash in reading

--- components/Entry/Reading.tsx
+++ components/Entry/Reading.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const READING_REG_EXP = /([<>]|\[.*?]|\s)/g;
+const READING_REG_EXP = /([/<>]|\[.*?]|\s)/g;
 
 export function Reading({ reading }: { reading: string }): JSX.Element {
   return <>{reading.replaceAll(READING_REG_EXP, "")}</>;
