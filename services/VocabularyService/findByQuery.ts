d1187f723985dec803e7c65657b9b6afb67b167f
Thorben Schiller <schillerthorben@gmail.com>
2021-07-23 16:24:14 +0200

    feat: search for direct hits against text_plain and hiragana_plain

--- services/VocabularyService/findByQuery.ts
+++ services/VocabularyService/findByQuery.ts
@@ -8,12 +8,25 @@ export async function findByQuery(
 ): Promise<EntryModel[]> {
   const { limit = DEFAULT_LIMIT, offset = 0 } = options;
   const results = await execute<EntryWrapperModel>(
-    `SELECT * FROM entry
-    WHERE
-        MATCH(text_plain, hiragana_plain, orths_plain, senses_plain)
-        AGAINST (? IN BOOLEAN MODE) LIMIT ?, ?`,
-    // parameters seem to be strings, @see https://github.com/sidorares/node-mysql2/issues/1239
-    [query, offset.toString(), Math.min(limit, MAX_LIMIT).toString()]
+    `SELECT *
+           FROM entry
+           WHERE MATCH(text_plain, hiragana_plain, orths_plain, senses_plain)
+                       AGAINST(? IN BOOLEAN MODE)
+           UNION
+           DISTINCT
+           SELECT *
+           FROM entry
+           WHERE text_plain = ?
+              OR hiragana_plain = ?
+           LIMIT ?, ?`,
+    // parameters seem to be strings, even for limit @see https://github.com/sidorares/node-mysql2/issues/1239
+    [
+      query,
+      query,
+      query,
+      offset.toString(),
+      Math.min(limit, MAX_LIMIT).toString(),
+    ]
   );
 
   return results.map((result) => result.entry_json);
