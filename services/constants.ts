063edb2eb569857ab86eef0f5d3cb73e4e37d931
Thorben Schiller <schillerthorben@gmail.com>
2021-07-24 10:09:41 +0200

    feat: allow configuration of limits via env

--- services/constants.ts
+++ services/constants.ts
@@ -1,3 +1,2 @@
-export const MAX_LIMIT = 15;
-
-export const DEFAULT_LIMIT = 15;
+export const MAX_LIMIT = Number(process.env.MAX_LIMIT) || 15;
+export const DEFAULT_LIMIT = Number(process.env.DEFAULT_LIMIT) || 15;
