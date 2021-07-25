e90c3d6ecafb611f728892a7d191b78d57fea13a
Thorben Schiller <schillerthorben@gmail.com>
2021-07-25 11:56:43 +0200

    feat: add opensearch

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { SITE_NAME } from "./_app";
 
 export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="de">
-        <Head />
+        <Head>
+          <link
+            rel="search"
+            type="application/opensearchdescription+xml"
+            title={`${SITE_NAME} Suche`}
+            href="/opensearch.xml"
+          />
+        </Head>
         <body className="bg-white text-black dark:bg-gray-900 dark:text-white font-normal">
           <Main />
           <NextScript />
