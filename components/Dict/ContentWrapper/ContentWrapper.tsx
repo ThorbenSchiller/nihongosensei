1a1b8f276fb403049154460041cfbf3aa1b5921a
thorbeN <schillerthorben@gmail.com>
2022-02-05 09:23:21 +0100

    fix: use padding instead of margin in footer

--- components/Dict/ContentWrapper/ContentWrapper.tsx
+++ components/Dict/ContentWrapper/ContentWrapper.tsx
@@ -13,7 +13,7 @@ export function ContentWrapper({ children }: ContentWrapperProps): JSX.Element {
     >
       <Header className="mt-4 mb-8" />
       <main className="flex-grow">{children}</main>
-      <Footer className="my-4 text-center" />
+      <Footer className="py-4 text-center" />
     </div>
   );
 }
