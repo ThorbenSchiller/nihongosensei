2857f01da3659f83c356d58a91aa16a7f24b6909
thorbeN <schillerthorben@gmail.com>
2023-03-01 18:16:40 +0100

    fix: prevent shrinking of logo

--- components/ui/Header/Header.tsx
+++ components/ui/Header/Header.tsx
@@ -11,7 +11,7 @@ export function Header({ className }: HeaderProps): JSX.Element {
   return (
     <header className={clsx("relative flex", className)}>
       <Link href="/" color="text" className="flex items-center mr-8">
-        <Logo />
+        <Logo className="shrink-0" />
       </Link>
       <Search className="mx-auto flex-grow" />
     </header>
